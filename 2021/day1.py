194a7669d4f72d1e6819bd8d9ee9d6a5b595d04f
Bhany <bhany@MB.local>
2022-03-04 21:55:52 -0800

    bug removed

--- 2021/day1.py
+++ 2021/day1.py
@@ -9,7 +9,7 @@ with open('day1_input.txt','r') as file:
         pre = curr
         curr = file.readline()
 
-# -----------------------------------
+# ----------------------------------
 # Part 2
 with open('day1_input.txt','r') as file:
     win_1 = file.readline()
